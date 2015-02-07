cac1a3f86354ee28db659db768fa231f674609f9
Jake Kinsella <jake.kinsella@gmail.com>
2015-02-07 14:51:15 -0500

    Add some comments on I2C stuff

--- src/org/usfirst/frc/team4761/robot/GyroSensor.java
+++ src/org/usfirst/frc/team4761/robot/GyroSensor.java
@@ -2,8 +2,14 @@ package org.usfirst.frc.team4761.robot;
 
 import edu.wpi.first.wpilibj.I2C;
 
+/*
+ * IMPORTANT!!!
+ * You will probably not understant this code and I am not going to make any attempt to put understandable comments on it.
+ * Ask Jake Kinsella if you really need an explanation of this code
+*/
+
 public class GyroSensor {
-	private static double degrees = 0;
+	private static double degrees = 0; // The accumulated degrees from the get rotations per second function
 	
 	private I2C gyro = RobotMap.gyro;
 	
@@ -15,9 +21,9 @@ public class GyroSensor {
 	
 	public double getDegrees (double deltaTime) {
 		byte[] angle = new byte[1];
-		gyro.read(0x47, 1, angle);
+		gyro.read(0x47, 1, angle); // Ony read from on register (I don't know why the other one doesn't work)
 		
-		double rotation = (angle[0] * deltaTime) * 2;
+		double rotation = (angle[0] * deltaTime) * 2; // I don't know why * 2 makes this work
 		
 		degrees += rotation;
 		
