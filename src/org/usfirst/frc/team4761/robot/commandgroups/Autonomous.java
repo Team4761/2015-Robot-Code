08179a48de5b337d4d74f257964349f24fb5544d
Jake Kinsella <jake.kinsella@gmail.com>
2015-02-10 15:35:08 -0500

    Add new command group to auto-mode

--- src/org/usfirst/frc/team4761/robot/commandgroups/Autonomous.java
+++ src/org/usfirst/frc/team4761/robot/commandgroups/Autonomous.java
@@ -12,6 +12,6 @@ public class Autonomous extends CommandGroup {
     
     public  Autonomous () {
     	addParallel(new LogToFile());
-    	//addSequential(new DriveForward());
+    	addSequential(new RcPickUp());
     }
 }
