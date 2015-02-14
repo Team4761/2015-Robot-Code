c77b22206a0b733b1e3a507267aee13bb1cf5733
jaredwhitney <jaredwhitney3@gmail.com>
2015-02-13 20:27:27 -0500

    Add MoveRcToLoad Command

---
+++ src/org/usfirst/frc/team4761/robot/commands/MoveRcToLoaded.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc.team4761.robot.commands;
+
+import org.usfirst.frc.team4761.robot.Robot;
+import org.usfirst.frc.team4761.robot.RobotMap;
+import org.usfirst.frc.team4761.robot.sensors.ShortDistanceSensor;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class MoveRcToLoaded extends Command {
+
+	ShortDistanceSensor loadSensor = RobotMap.conveyorDistanceSensor;
+	
+	protected void initialize() {
+		requires(Robot.mainConveyorBelt);
+	}
+
+	protected void execute() {
+		Robot.mainConveyorBelt.go(0.5);
+	}
+
+	protected boolean isFinished() {
+		if (loadSensor.getDistance() < 30)	// if the sensor reads less than 30cm, it is far less than 
+			return true;
+		return false;
+	}
+
+	protected void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
