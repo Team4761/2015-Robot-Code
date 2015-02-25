abc2a793fe11ec3d9b89f2f97d8f89692d6098e7
Jake Kinsella <jake.kinsella@gmail.com>
2015-02-24 21:16:20 -0500

    Reformatted RobotMap to compile with standards

--- src/org/usfirst/frc/team4761/robot/RobotMap.java
+++ src/org/usfirst/frc/team4761/robot/RobotMap.java
@@ -16,67 +16,102 @@ import org.usfirst.frc.team4761.robot.sensors.ShortDistanceSensor;
  * floating around.
  */
 public class RobotMap {
-	public static VictorSP leftFrontMotor = new VictorSP(3);
-	public static VictorSP leftRearMotor = new VictorSP(1);
-	public static VictorSP rightFrontMotor = new VictorSP(2);
-	public static VictorSP rightRearMotor = new VictorSP(0);
+	public static VictorSP leftFrontMotor;
+	public static VictorSP leftRearMotor;
+	public static VictorSP rightFrontMotor;
+	public static VictorSP rightRearMotor;
 	
+	public static RobotDrive robotDrive;
 	
-	public static RobotDrive robotDrive = new RobotDrive(leftFrontMotor, leftRearMotor, rightFrontMotor, rightRearMotor);
+	public static GyroSensor gyro;
 	
-	// New Gyro
-	public static final GyroSensor gyro = new GyroSensor();
-	
-	/**
-	 * A distance sensor that senses the barrels for auto-mode.
-	 */
-	public static final MediumDistanceSensor barrelDistanceSensor = new MediumDistanceSensor(new AnalogInput(0));
-	
-	/**
-	 * A distance sensor on the elevator.
-	 */
-	public static final MediumDistanceSensor elevatorDistanceSensor = new MediumDistanceSensor(new AnalogInput(1));
-	
-	/**
-	 * Distance sensor on the rear of the outer conveyor belt. Used for
-	 * detecting something. TODO: Figure out what it's use is. Previously elevatorBarrelDistanceSensor
-	 */
-	public static final MediumDistanceSensor outerConveyorToteDistanceSensor = new MediumDistanceSensor(new AnalogInput(2));
-	
-	/**
-	 * Distance sensor on the front of the outer conveyor belt. Used for
-	 * detecting when the elevator is at the top of a tote stack. Previously elevatorToteDistanceSensor
-	 */
-	public static final ShortDistanceSensor outerConveyorBarrelDistanceSensor = new ShortDistanceSensor(new AnalogInput(3));
+	public static MediumDistanceSensor barrelDistanceSensor;
+	public static MediumDistanceSensor elevatorDistanceSensor;
+	public static MediumDistanceSensor outerConveyorToteDistanceSensor;
 
-	public static Logger log = new Logger("4761", LoggingMode.LOG, "/home/lvuser/log.txt"); // Create an instance of our logging program
-	public static Level minLogLevel = Level.DEV;
+	public static ShortDistanceSensor outerConveyorBarrelDistanceSensor;
 	
-	public static DoubleSolenoid rcPneumatic = new DoubleSolenoid(0, 0, 1);
-	public static DoubleSolenoid plowPneumatic = new DoubleSolenoid(0, 6, 7);
+	public static Logger log;
+	public static Level minLogLevel;
 	
-	// New Robot
-	public static VictorSP spinner = new VictorSP(4);
+	public static DoubleSolenoid rcPneumatic;
+	public static DoubleSolenoid plowPneumatic;
 	
-	/**
-	 * Digital inputs for the limit switches of the rcGrabberBase.</br>
-	 */
-	public static DigitalInput spinnerDI1 = new DigitalInput(8);
-	public static DigitalInput spinnerDI2 = new DigitalInput(9);
+	public static VictorSP spinner;
 	
-	/**
-	 * Digital inputs for the limit switches for the side of the robot.</br>
-	 */
-	public static DigitalInput sideDI1 = new DigitalInput(0);
-	public static DigitalInput sideDI2 = new DigitalInput(1);
+	public static DigitalInput spinnerDI1;
+	public static DigitalInput spinnerDI2;
 	
-	/**
-	 * Digital inputs for the limit switch on the bottom of the elevator
-	 */
-	public static DigitalInput elevatorDI = new DigitalInput(2);
+	public static DigitalInput elevatorDI;
 	
-	public static Talon mainConveyorBeltMotor = new Talon(6);
-	public static VictorSP elevatorConveyorBeltMotor = new VictorSP(7);
-	public static VictorSP elevatorMotor1 = new VictorSP(8);
-	public static VictorSP elevatorMotor2 = new VictorSP(9);
+	public static Talon mainConveyorBeltMotor;
+	public static VictorSP elevatorConveyorBeltMotor;
+	public static VictorSP elevatorMotor1;
+	public static VictorSP elevatorMotor2;
+	
+	public RobotMap () {
+		leftFrontMotor = new VictorSP(3);
+		leftRearMotor = new VictorSP(1);
+		rightFrontMotor = new VictorSP(2);
+		rightRearMotor = new VictorSP(0);
+		
+		
+		robotDrive = new RobotDrive(leftFrontMotor, leftRearMotor, rightFrontMotor, rightRearMotor);
+		
+		// New Gyro
+		gyro = new GyroSensor();
+		
+		/**
+		 * A distance sensor that senses the barrels for auto-mode.
+		 */
+		barrelDistanceSensor = new MediumDistanceSensor(new AnalogInput(0));
+		
+		/**
+		 * A distance sensor on the elevator.
+		 */
+		elevatorDistanceSensor = new MediumDistanceSensor(new AnalogInput(1));
+		
+		/**
+		 * Distance sensor on the rear of the outer conveyor belt. Used for
+		 * detecting something. TODO: Figure out what it's use is. Previously elevatorBarrelDistanceSensor
+		 */
+		outerConveyorToteDistanceSensor = new MediumDistanceSensor(new AnalogInput(2));
+		
+		/**
+		 * Distance sensor on the front of the outer conveyor belt. Used for
+		 * detecting when the elevator is at the top of a tote stack. Previously elevatorToteDistanceSensor
+		 */
+		outerConveyorBarrelDistanceSensor = new ShortDistanceSensor(new AnalogInput(3));
+	
+		log = new Logger("4761", LoggingMode.LOG, "/home/lvuser/log.txt"); // Create an instance of our logging program
+		minLogLevel = Level.DEV;
+		
+		rcPneumatic = new DoubleSolenoid(0, 0, 1);
+		plowPneumatic = new DoubleSolenoid(0, 6, 7);
+		
+		// New Robot
+		spinner = new VictorSP(4);
+		
+		/**
+		 * Digital inputs for the limit switches of the rcGrabberBase.</br>
+		 */
+		spinnerDI1 = new DigitalInput(8);
+		spinnerDI2 = new DigitalInput(9);
+		
+		/**
+		 * Digital inputs for the limit switches for the side of the robot (NOT IMPLEMENTED!!!).</br>
+		 */
+		/*public static DigitalInput sideDI1 = new DigitalInput(0);
+		public static DigitalInput sideDI2 = new DigitalInput(1);*/
+		
+		/**
+		 * Digital input for the limit switch on the bottom of the elevator
+		 */
+		elevatorDI = new DigitalInput(2);
+		
+		mainConveyorBeltMotor = new Talon(6);
+		elevatorConveyorBeltMotor = new VictorSP(7);
+		elevatorMotor1 = new VictorSP(8);
+		elevatorMotor2 = new VictorSP(9);
+	}
 }
