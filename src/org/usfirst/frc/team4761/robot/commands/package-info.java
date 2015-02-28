962cf9de87371443e5f7c8146d4351b331e444bf
Simon Andrews <simon.andrews212@gmail.com>
2015-02-28 18:01:59 -0500

    Document commands package

--- src/org/usfirst/frc/team4761/robot/commands/package-info.java
+++ src/org/usfirst/frc/team4761/robot/commands/package-info.java
@@ -1,5 +1,7 @@
 /**
- * Commands that do exactly one thing each. Can be strung together with command
- * groups or run by themselves.
+ * Commands that do exactly one thing each. This package is only for stuff that
+ * doesn't fit into the other subpackages. An example of this might be a
+ * command which requires multiple subsystems (though if this is the case you
+ * should probably be looking at using a command group instead).
  */
 package org.usfirst.frc.team4761.robot.commands;
