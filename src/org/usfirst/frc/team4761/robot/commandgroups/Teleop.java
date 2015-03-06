dc528adee41f1220788863cb1eac42bd30c07fc5
Simon Andrews <simon.andrews212@gmail.com>
2015-03-05 20:17:29 -0500

    Fix class names

--- src/org/usfirst/frc/team4761/robot/commandgroups/Teleop.java
+++ src/org/usfirst/frc/team4761/robot/commandgroups/Teleop.java
@@ -3,9 +3,8 @@ package org.usfirst.frc.team4761.robot.commandgroups;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 import org.usfirst.frc.team4761.robot.buttons.Rumble;
-import org.usfirst.frc.team4761.robot.commands.LEDs;
+import org.usfirst.frc.team4761.robot.commands.ControlLeds;
 import org.usfirst.frc.team4761.robot.commands.LogToFile;
-import org.usfirst.frc.team4761.robot.commands.drivetrain.DriveWithJoysticks;
 
 /**
  * The command that is run when the robot enters teleop mode.
@@ -14,7 +13,7 @@ public class Teleop extends CommandGroup {
 	
 	public Teleop() {
 		addParallel(new LogToFile());
-		addParallel(new LEDs());
+		addParallel(new ControlLeds());
 		addParallel(new Rumble(1, 1)); // Random numbers just for testing
 		//addParallel(new DriveWithJoysticks());
 	}
