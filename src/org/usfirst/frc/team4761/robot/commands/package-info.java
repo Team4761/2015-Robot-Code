2374ff7028635b699551509f3ae05322d0efbf27
Simon Andrews <simon.andrews212@gmail.com>
2015-03-06 09:49:43 -0500

    Document remaining packages.

---
+++ src/org/usfirst/frc/team4761/robot/commands/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Catch-all package for commands that don't really fit into any of the other
+ * command subpackages.
+ */
+package org.usfirst.frc.team4761.robot.commands;
---
+++ src/org/usfirst/frc/team4761/robot/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Catch-all for classes that don't really go anywhere else. Includes mappings,
+ * useful utilities, and other neat stuff. If you don't know where your thing
+ * goes, it goes here.
+ */
+package org.usfirst.frc.team4761.robot;
