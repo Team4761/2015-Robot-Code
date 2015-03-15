4a066bfc4b696333f30328f71a42e40f577a9407
Jake Kinsella <jake.kinsella@gmail.com>
2015-03-15 12:13:42 -0400

    Hopefully made the move elevator to stack top command better

--- src/org/usfirst/frc/team4761/robot/commands/elevator/MoveElevatorToStackTop.java
+++ src/org/usfirst/frc/team4761/robot/commands/elevator/MoveElevatorToStackTop.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class MoveElevatorToStackTop extends Command {
 	MediumDistanceSensor sensor = RobotMap.outerConveyorToteDistanceSensor;
+	private boolean passedStack = false;
 	
 	public MoveElevatorToStackTop() {
 		requires(Robot.elevator);
@@ -23,12 +24,19 @@ public class MoveElevatorToStackTop extends Command {
 	
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		Robot.elevator.raise();
+		if (sensor.getDistance() > 80 && !passedStack) {
+			passedStack = true;
+		}
+		if (passedStack) {
+			Robot.elevator.lower();
+		} else {
+			Robot.elevator.raise();
+		}
 	}
 	
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return sensor.getDistance() > 80; // Stop running when past last tote in stack
+		return sensor.getDistance() < 80 && passedStack; // Stop running when past last tote in stack
 	}
 	
 	// Called once after isFinished returns true
