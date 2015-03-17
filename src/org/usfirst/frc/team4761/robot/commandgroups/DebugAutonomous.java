352b20edec703f0bd755e1307a65672094a304f5
Simon Andrews <simon.andrews212@gmail.com>
2015-03-16 20:58:46 -0400

    Add thing for debugging distance sensors

--- src/org/usfirst/frc/team4761/robot/commandgroups/DebugAutonomous.java
+++ src/org/usfirst/frc/team4761/robot/commandgroups/DebugAutonomous.java
@@ -1,13 +1,18 @@
 package org.usfirst.frc.team4761.robot.commandgroups;
 
+import org.usfirst.frc.team4761.robot.commands.debug.ReadDistanceSensor;
+import org.usfirst.frc.team4761.robot.sensors.MediumDistanceSensor;
+
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
- * Adds a 'ReadDi0Value' command to currently executing command group. 
+ * Autonomous for debugging stuff. Feel free to delete whatever is currently in
+ * here. If you put something in here, do not expect it to stay.
  */
 public class DebugAutonomous extends CommandGroup {
-    
     public  DebugAutonomous() {
-
+    	//TODO: replace with real port
+    	addSequential(new ReadDistanceSensor(new MediumDistanceSensor(new AnalogInput(0))));
     }
 }
---
+++ src/org/usfirst/frc/team4761/robot/commands/debug/ReadDistanceSensor.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team4761.robot.commands.debug;
+
+import org.usfirst.frc.team4761.robot.sensors.DistanceSensor;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Reads distance and voltage from the distance sensor every 20ms and prints it
+ * to the console.
+ */
+public class ReadDistanceSensor extends Command {
+	private final DistanceSensor distanceSensor;
+    public ReadDistanceSensor(DistanceSensor distanceSensor) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	this.distanceSensor = distanceSensor;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	System.out.println(String.format("DISTANCE: %d | VOLTAGE: %d",
+    			distanceSensor.getDistance(),
+    			distanceSensor.getVoltage()));
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
