0a6dfee2dce86b992b3ead16f4b34ae99d9b2590
Jake Kinsella <jake.kinsella@gmail.com>
2015-04-07 19:53:52 -0400

    Code cleanups and add more stuff to RcArm

--- src/org/usfirst/frc/team4761/robot/subsystems/RcArm.java
+++ src/org/usfirst/frc/team4761/robot/subsystems/RcArm.java
@@ -3,13 +3,15 @@ package org.usfirst.frc.team4761.robot.subsystems;
 import org.usfirst.frc.team4761.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  * This is a arm on the robot that will grab recycling bins off the step. This is not on the robot yet.
  */
 public class RcArm extends Subsystem {
-	Servo servo = RobotMap.servo;
+	private Servo servo = RobotMap.servo;
+	private VictorSP winch = RobotMap.winch;
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
@@ -19,7 +21,15 @@ public class RcArm extends Subsystem {
         //setDefaultCommand(new MySpecialCommand());
     }
     
-    public void goToAngle (double angle) {
+    public void in () {
+    	winch.set(1);
+    }
+    
+    public void out () {
+    	winch.set(-1);
+    }
+    
+    public void servoToAngle (double angle) {
     	servo.setAngle(angle);
     }
 }
