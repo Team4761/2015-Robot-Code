59270ca2211642272d308491d43b1da6f3c83612
Jake Kinsella <jake.kinsella@gmail.com>
2015-04-07 19:04:35 -0400

    Remove DriveLeft command

--- src/org/usfirst/frc/team4761/robot/commandgroups/ThreeBarrelAutonomous.java
+++ src/org/usfirst/frc/team4761/robot/commandgroups/ThreeBarrelAutonomous.java
@@ -1,12 +1,12 @@
 package org.usfirst.frc.team4761.robot.commandgroups;
 
 import org.usfirst.frc.team4761.robot.buttons.ResetGyro;
+import org.usfirst.frc.team4761.robot.commands.drivetrain.Drive;
 import org.usfirst.frc.team4761.robot.commands.drivetrain.GoToBackEdgeOfBarrel;
 import org.usfirst.frc.team4761.robot.commands.drivetrain.GoToNextBarrel;
 import org.usfirst.frc.team4761.robot.commands.Stop;
 import org.usfirst.frc.team4761.robot.commands.conveyorbelts.MainConveyorForward;
 import org.usfirst.frc.team4761.robot.commands.debug.LogToFile;
-import org.usfirst.frc.team4761.robot.commands.drivetrain.DriveLeft;
 import org.usfirst.frc.team4761.robot.commands.rcgrabber.RcGrabberUp;
 import org.usfirst.frc.team4761.robot.commands.rcgrabber.SpinRcBaseOut;
 
@@ -25,13 +25,13 @@ public class ThreeBarrelAutonomous extends CommandGroup {
     	addParallel(new MainConveyorForward());
     	addSequential(new RcPickUp());
 		addParallel(new SpinRcBaseOut());
-        addParallel(new DriveLeft());
+        addParallel(new Drive(0.15, 0, 90, 0, "ABSOLUTE"));
         addSequential(new GoToNextBarrel());
         addSequential(new GoToBackEdgeOfBarrel());
         addSequential(new Stop());
 		addSequential(new RcPickUp());
 		addParallel(new SpinRcBaseOut());
-		addParallel(new DriveLeft());
+        addParallel(new Drive(0.15, 0, 90, 0, "ABSOLUTE"));
 		addSequential(new GoToNextBarrel());
 		addSequential(new GoToBackEdgeOfBarrel());
 		addSequential(new Stop());
