5762da7046d898b108a9ab689658d3ac1c4891d1
Jake Kinsella <jake.kinsella@gmail.com>
2015-04-07 20:12:44 -0400

    Add RcArm stuff

---
+++ src/org/usfirst/frc/team4761/robot/commands/rcarm/RcArmMove.java
@@ -0,0 +1,112 @@
+package org.usfirst.frc.team4761.robot.commands.rcarm;
+
+import org.usfirst.frc.team4761.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class RcArmMove extends Command {
+	boolean isServo = false;
+	double move = 0, timeout;
+	
+	public RcArmMove (RcArmType type) {
+		requires(Robot.rcArm);
+		
+    	switch (type) {
+    		case IN:
+    			isServo = false;
+    			move = 1;
+    			break;
+    		case OUT:
+    			isServo = false;
+    			move = -1;
+    			break;
+    		case STOP:
+    			isServo = false;
+    			move = 0;
+    			break;
+    		case SERVO:
+    			isServo = true;
+    			move = 90;
+    			break;
+    	}
+    }
+	
+    public RcArmMove (RcArmType type, double timeout) {
+		requires(Robot.rcArm);
+
+    	switch (type) {
+    		case IN:
+    			isServo = false;
+    			move = 1;
+    			break;
+    		case OUT:
+    			isServo = false;
+    			move = -1;
+    			break;
+    		case STOP:
+    			isServo = false;
+    			move = 0;
+    			break;
+    		case SERVO:
+    			isServo = true;
+    			move = 90;
+    			break;
+    	}
+    	
+    	this.timeout = timeout;
+    }
+    
+    public RcArmMove (double angle, double timeout) {
+		requires(Robot.rcArm);
+
+    	isServo = true;
+    	this.move = angle;
+    	
+    	this.timeout = timeout;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize () {
+    	setTimeout(timeout);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute () {
+    	if (isServo) {
+    		Robot.rcArm.servoToAngle(move);
+    	} else {
+    		if (move == 1) {
+    			Robot.rcArm.in();
+    		} else if (move == -1) {
+    			Robot.rcArm.out();
+    		} else {
+    			Robot.rcArm.stop();
+    		}
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished () {
+    	if (timeout != 0) {
+    		return isTimedOut();
+    	}
+    	
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end () {
+    	if (!isServo) {
+    		Robot.rcArm.stop();
+    	}
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted () {
+    	end();
+    }
+}
---
+++ src/org/usfirst/frc/team4761/robot/commands/rcarm/RcArmType.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc.team4761.robot.commands.rcarm;
+
+public enum RcArmType {
+	IN,
+	OUT,
+	STOP,
+	SERVO
+}
